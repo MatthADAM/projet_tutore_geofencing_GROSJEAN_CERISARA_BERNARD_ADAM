4eeb51f2c3ca0f27b78eb00b4d6f90c48e0f5784
Matthieu ADAM <matthieu.adam(@etu.univ-lorraine.fr>
2021-01-08 15:10:32 +0100

    getUsers by id api ok

--- geofencingW/app/routes.php
+++ geofencingW/app/routes.php
@@ -5,32 +5,35 @@ use \App\Application\Actions\AccueilAction as AccueilAction;
 use \App\Application\Actions\User\ComptesConnexionAction as ComptesConnexionAction;
 use \App\Application\Actions\User\ComptesDeconnexionAction as ComptesDeconnexionAction;
 use \App\Application\Actions\User\ComptesInscriptionAction as ComptesInscriptionAction;
-use \App\Application\Actions\Api\ListUsersAction as ListUsersAction;
 use \App\Application\Actions\User\MonCompteAction as MonCompteAction;
 use \App\Application\Actions\User\ModifierInfosUtilisateurAction as ModifierInfosUtilisateurAction;
+
+use \App\Application\Actions\Api\ListUsersAction;
+use \App\Application\Actions\Api\GetUsersAction;
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
-
+    
     $app->get('/', AccueilAction::class)->setName("getAccueil");;
-
+    
     /* Connexion */
-
+    
     $app->get('/connexion', ComptesConnexionAction::class)->setName("getConnexion");
-
+    
     $app->post('/connexion', ComptesConnexionAction::class)->setName("postConnexion");
-
+    
     $app->post('/deconnexion', ComptesDeconnexionAction::class)->setName("postDeconnexion");
-
+    
     /* Inscription */
-
+    
     $app->get('/inscription', ComptesInscriptionAction::class)->setName("getInscription");
-
+    
     $app->post('/inscription', ComptesInscriptionAction::class)->setName("postInscription");
-
+    
     //update User
     $app->get('/updateUser/{id}',ModifierInfosUtilisateurAction::class)->setName("getUpdateUser");
     $app->post('/updateUser/{id}',ModifierInfosUtilisateurAction::class)->setName("postUpdateUser");
@@ -38,6 +41,9 @@ return function (App $app) {
     //voir son compte
     $app->get('/monCompte/{id}',MonCompteAction::class)->setName("getMonCompte");
     $app->post('/monCompte/{id}',MonCompteAction::class)->setName("postMonCompte");
-
-    $app->get('/api',ListUsersAction::class)->setName("getUsers");
+    
+    $app->group('/api', function (Group $group) {
+        $group->get('/users',ListUsersAction::class)->setName("getUsers");
+        $group->get('/users/{id}', GetUsersAction::class);
+    });
 };
