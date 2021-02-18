77ebf6f83bf750771e2d5d58bd16a4f196836885
theo <theo@pop-os.localdomain>
2021-02-18 10:07:48 +0100

    modif taille map

--- app_geofencing/lib/pages/MapPage.dart
+++ app_geofencing/lib/pages/MapPage.dart
@@ -216,7 +216,7 @@ class _MapPageState extends State<MapPage> {
     _listenLocation();
     if (res.length > 0) {
       return SizedBox(
-        height: MediaQuery.of(context).size.height * 0.869,
+        height: MediaQuery.of(context).size.height * 0.868,
         child: new FlutterMap(
           options: new MapOptions(
             center: new LatLng(/* 48.6309538, 6.1067854 */ _location.latitude,
